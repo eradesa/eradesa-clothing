b0ea43056f88aecfabd2b1fffe97767fca38c434
eradesa <eranga.de.saram@gmail.com>
2022-09-08 10:12:02 +0530

    with redirect

--- src/routes/sign-in/sign-in.components.jsx
+++ src/routes/sign-in/sign-in.components.jsx
@@ -1,7 +1,21 @@
-import { signInWithGooglePopup,
-        creatUserDocumentFromAuth 
+import { useEffect } from 'react';
+import { getRedirectResult} from 'firebase/auth'
+import { 
+    auth,
+    signInWithGooglePopup,
+    signInWithGoogleRedirect,
+    creatUserDocumentFromAuth 
 } from '../../utils/firebase/firebase.utils'
-const SignIn = () => {
+const SignIn =  () => {
+
+    useEffect (() => async ()=> {
+        const response  = await getRedirectResult(auth);
+        console.log(response);
+        if (response) {
+            const userDocRef =  await creatUserDocumentFromAuth(response.user);
+
+        } 
+    },[]);
     const logGoogleUser = async () => {
         //const response = await signInWithGooglePopup(); //-Method 01
         //creatUserDocumentFromAuth(response.user);
@@ -9,13 +23,13 @@ const SignIn = () => {
         const userDocRef =  await creatUserDocumentFromAuth(user);
         //console.log (response)
 
-    }
+    };
+
     return (
         <div>
             <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>
-                Sign in with Google Popup
-            </button>
+            <button onClick={logGoogleUser}>Sign in with Google Popup </button>
+            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect </button>
         </div>
     )
 
