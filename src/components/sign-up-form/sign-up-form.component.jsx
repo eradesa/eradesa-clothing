eae2389f918f9551fbcf7cbe5d9a418d3d1941ff
eradesa <eranga.de.saram@gmail.com>
2022-09-09 04:03:26 +0530

    After Lesson 100

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -55,17 +55,42 @@ const SignUpForm = () =>{
         <div>
             <h1>Sign up with your email and password</h1>
             <form onSubmit={handleSubmit}>
-                <label>Display Name</label>
-                <input type="test" required onChange={handleChange} name="displayName" value={displayName}/>
+                <FormInput
+                    label="Display Name"
+                    type="text"
+                    required 
+                    onChange={handleChange} 
+                    name="displayName" 
+                    value={displayName}
+                />
 
-                <label>Email</label>
-                <input type="email" required onChange={handleChange} name="email" value={email}/>
+                <FormInput
+                    label="Email"
+                    type="email"
+                    required 
+                    onChange={handleChange} 
+                    name="email" 
+                    value={email}
+                />
+
+                <FormInput
+                    label="Password"
+                    type="password"
+                    required 
+                    onChange={handleChange} 
+                    name="password" 
+                    value={password}
+                />
+
+                <FormInput
+                    label="Confirm Password"
+                    type="password"
+                    required 
+                    onChange={handleChange} 
+                    name="confirmPassword" 
+                    value={confirmPassword}
+                />                          
                 
-                <label >Password</label>
-                <input type="password" required onChange={handleChange} name="password" value={password}/>
-
-                <label>Confirm Password</label>
-                <input type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword}/>
                 <button type="submit">Sign Up </button>
             </form>
         </div>
